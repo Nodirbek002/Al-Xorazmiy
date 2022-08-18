b05a67a3b8941877f66883ddbf4ae8e46acc65b0
nodirbek <nodirbekazizjonov777@gmail.com>
2022-08-18 14:48:43 -0700

    nodirbek

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+    <div className="app">
+      <h1>Hello world</h1>
+    </div>
+   </>
+  )
+}
+
+export default App
