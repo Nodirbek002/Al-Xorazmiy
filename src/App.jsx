f41a51fda946083ab0462c08f89942fc2a386686
bobur <bobur02k@gmail.com>
2022-08-19 03:07:07 +0300

    bobur

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
     <div className="app">
       <h1>Hello world</h1>
+     <h1>bobur</h1>
     </div>
    </>
   )
